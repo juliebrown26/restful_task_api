a0f85c5458f5c80bb11bce3b94549f9c0ff7e43d
Julie Brown <juliebrown26@gmail.com>
2019-08-15 12:12:18 -0600

    init commit

---
+++ server.js
@@ -0,0 +1,13 @@
+// Import
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+//Config
+// require('./server/routes/mongoose');
+app.use(express.static(__dirname + "/public/dist/public"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+//Routes
+// require('./server/routes/name.routes')(app);
+//Port
+app.listen(8000, () => console.log("listening on port 8000"));
